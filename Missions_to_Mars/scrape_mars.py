70336bfa1fcd4caf46d561051d2497be57f915f2
Franco1230 <franco.hfh1230@gmail.com>
2021-12-05 12:19:09 +1100

    updated to Bushfire news

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -20,10 +20,10 @@ def scrape():
     browser = init_browser()
     
 
-    """ NASA Mars News """
-    # Visit to Mars News Site
-    mars_news_url = "http://redplanetscience.com"
-    browser.visit(mars_news_url)
+    """ 9News Bushfire News """
+    # Visit to 9News Site
+    bushfire_news_url = "https://www.9news.com.au/bushfires"
+    browser.visit(bushfire_news_url)
 
     # HTML Object
     html = browser.html
@@ -32,12 +32,12 @@ def scrape():
     news_soup = bs(html, parser)    
 
     # Retrieve the latest article's title
-    news_title = news_soup.find("div", class_ = "content_title")
+    news_title = news_soup.find("h3", class_ = "story__headline")
     news_title = news_title.text.strip()
     print(news_title)
 
     # Retrieve the latest article's paragraph
-    news_paragraph = news_soup.find("div", class_ = "article_teaser_body")
+    news_paragraph = news_soup.find("div", class_ = "story__abstract")
     news_paragraph = news_paragraph.text.strip()
     print(news_paragraph)
 

fee0a08493e17d5ec6de834bea9c169af7a853fc
Franco1230 <franco.hfh1230@gmail.com>
2021-12-11 21:30:50 +1100

    a few changes in app.py - still not working

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 # Dependencies
 
 from sqlalchemy import inspect
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 # Create an instance of Flask
+import scrape_fire
 import os
 import psycopg2
 from sqlalchemy.inspection import inspect
-from sqlalchemy import create_engine
 import numpy as np
 import pandas as pd
 from flask import render_template
+from flask import Flask, jsonify
 # DATABASE_URL = ''
 # DATABASE_URL='postgresql://postgres:monash123@localhost/bushFire_db'
 # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -35,8 +36,10 @@ print(engine.execute('SELECT * FROM fire_location LIMIT 5').fetchall())
 # Reflect Database into ORM class
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-print(Base.classes.keys())
+
 fire_loc = Base.classes.fire_location
+
+app = Flask(__name__)
 
 # Start a session to query the database
 session = Session(engine)
@@ -45,7 +48,6 @@ results = session.query(fire_loc.latitude).all()
 # Unpack 
 all_names = list(np.ravel(results))
 print(all_names)
-
 
 # # # psycopg2
 # # # sqlAlchemy
@@ -74,23 +76,22 @@ def scrape():
     # Call to run the scrape function
     bushfire = scrape_fire.scrape()
 
-    # Update the Mongo DB each time when new scrape happen
-    mongo.db.bushfire.update({}, bushfire, upsert = True)
-
     # Back to the home page
-    return redirect("/", code = 302)
+    return jsonify(bushfire)
 
 # # # Route that will trigger the scrape function
 @app.route("/map")
 def map():
-    loc_table=session.query(fire_location).all()
-    return json(loc_table)
+    session = Session(engine)
+    loc_table = session.query(fire_loc).all()
+    session.close()
+    return jsonify(loc_table)
 
 # # # Route that will trigger the scrape function
 @app.route("/graph")
 def map():
-    loc_table=session.query(fire_location).all()##
-    return json(loc_table)
+    loc_table=session.query(fire_loc).all()##
+    return jsonify(loc_table)
 
 if __name__ == "__main__":
     app.run(debug = True)
